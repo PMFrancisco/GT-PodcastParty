f0feebd3fb1c5786fcb0afbe0283f4c4c4e95765
Francisco Pérez Muñoz <francisco.perez.munoz@gmail.com>
2024-10-09 18:10:36 +0200

    feat(server): cors for all routes

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require('express');
-const morgan = require('morgan')
+const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(morgan('dev'));
+app.use(cors());  // CORS habilitado para todas las rutas
+
+app.use("/", require("./routes"));
+
 app.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
-
-
-app.use(morgan("dev"));
-app.use("/", require("./routes"));
