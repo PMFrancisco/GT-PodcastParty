aece881f6cbcf0d115e087cc37f203483b02427b
Francisco Pérez Muñoz <francisco.perez.munoz@gmail.com>
2024-10-09 17:36:42 +0200

    feat(server): added get one episode by pub date

--- server/routes/episodes.js
+++ server/routes/episodes.js
@@ -1,30 +1,61 @@
-const express = require("express")
+const express = require("express");
 const router = express.Router();
-
-const Parser = require('rss-parser');
+const Parser = require("rss-parser");
 const parser = new Parser();
 
-router.get('/', async (req, res) => {
+const FEED_URL = "https://pod.link/1285264897.rss";
+
+const fetchFeed = async () => {
   try {
-    const feedUrl = 'https://pod.link/1285264897.rss';
-    const feed = await parser.parseURL(feedUrl);
+    return await parser.parseURL(FEED_URL);
+  } catch (error) {
+    throw new Error("Error al obtener el feed RSS");
+  }
+};
+
+router.get("/", async (req, res) => {
+  try {
+    const feed = await fetchFeed();
 
     if (feed.items && feed.items.length > 0) {
       const last20Episodes = feed.items.slice(0, 20);
 
-      const formattedEpisodes = last20Episodes.map(item => ({
+      const formattedEpisodes = last20Episodes.map((item) => ({
         title: item.title,
         link: item.link,
         pubDate: item.pubDate,
-        length: item.enclosure.length
+        length: item.enclosure.length,
       }));
 
       res.json(formattedEpisodes);
     } else {
-      res.status(404).json({ message: 'No se encontraron episodios' });
+      res.status(404).json({ message: "No se encontraron episodios" });
     }
   } catch (error) {
-    res.status(500).json({ message: 'Error al obtener los episodios', error });
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.get("/:pubDate", async (req, res) => {
+  try {
+    const feed = await fetchFeed();
+
+    const episode = feed.items.find(
+      (item) => item.pubDate === req.params.pubDate
+    );
+
+    if (episode) {
+      res.json({
+        title: episode.title,
+        link: episode.link,
+        pubDate: episode.pubDate,
+        length: episode.enclosure.length,
+      });
+    } else {
+      res.status(404).json({ message: "No se encontró el episodio" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
