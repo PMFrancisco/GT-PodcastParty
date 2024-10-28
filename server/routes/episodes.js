eaa95d914fd474230f8c748b7f6cbfc17fac8219
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-10-28 19:35:24 +0100

    docs(server): updated swagger

--- server/routes/episodes.js
+++ server/routes/episodes.js
@@ -24,52 +24,72 @@ const fetchFeed = async () => {
  * @swagger
  * /episodes:
  *   get:
- *     summary: Get the latest 20 episodes
+ *     summary: Get paginated episodes
+ *     description: Retrieve episodes in paginated format. Each page contains 20 episodes.
  *     tags: [Episodes]
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: Page number to retrieve (default is 1)
  *     responses:
  *       '200':
- *         description: A list of the latest 20 episodes
+ *         description: A list of episodes for the specified page
  *         content:
  *           application/json:
  *             schema:
- *               type: array
- *               items:
- *                 type: object
- *                 properties:
- *                   title:
- *                     type: string
- *                     example: "Episode Title"
- *                   link:
- *                     type: string
- *                     example: "http://example.com/episode"
- *                   pubDate:
- *                     type: string
- *                     format: date-time
- *                     example: "2023-10-01T00:00:00Z"
- *                   content:
- *                     type: string
- *                     example: "Episode content"
- *                   id:
- *                     type: string
- *                     example: "61641297"
- *                   duration:
- *                     type: string
- *                     example: "00:30:00"
- *                   audioInfo:
+ *               type: object
+ *               properties:
+ *                 currentPage:
+ *                   type: integer
+ *                   example: 1
+ *                 totalItems:
+ *                   type: integer
+ *                   example: 100
+ *                 totalPages:
+ *                   type: integer
+ *                   example: 5
+ *                 episodes:
+ *                   type: array
+ *                   items:
  *                     type: object
  *                     properties:
- *                       url:
+ *                       title:
  *                         type: string
- *                         example: "http://example.com/audio.mp3"
- *                       length:
+ *                         example: "Episode Title"
+ *                       link:
  *                         type: string
- *                         example: "12345678"
- *                       type:
+ *                         example: "http://example.com/episode"
+ *                       pubDate:
  *                         type: string
- *                         example: "audio/mpeg"
- *                   image:
- *                     type: string
- *                     example: "http://example.com/image.jpg"
+ *                         format: date-time
+ *                         example: "2023-10-01T00:00:00Z"
+ *                       content:
+ *                         type: string
+ *                         example: "Episode content"
+ *                       id:
+ *                         type: string
+ *                         example: "61641297"
+ *                       duration:
+ *                         type: string
+ *                         example: "00:30:00"
+ *                       audioInfo:
+ *                         type: object
+ *                         properties:
+ *                           url:
+ *                             type: string
+ *                             example: "http://example.com/audio.mp3"
+ *                           length:
+ *                             type: string
+ *                             example: "12345678"
+ *                           type:
+ *                             type: string
+ *                             example: "audio/mpeg"
+ *                       image:
+ *                         type: string
+ *                         example: "http://example.com/image.jpg"
  *       '404':
  *         description: No episodes found
  *         content:
@@ -91,6 +111,7 @@ const fetchFeed = async () => {
  *                   type: string
  *                   example: "Error al obtener el feed RSS"
  */
+
 
 router.get("/", async (req, res) => {
   try {
