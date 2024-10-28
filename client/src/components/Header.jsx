5a52b736491aebd7bd960a753ba4b5ef452ec31e
Sara Monzón Quesada <sara.mon.quesada@gmail.com>
2024-10-28 11:42:35 +0000

    feat(podcast-party): update header styles

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,27 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
-import './HomePage.css'
+import './Header.css'
+import logo from '../assets/logopng.png';
 
 const Header = () => {
   return (
     <header className="header">
-      {/* Logo */}
-      <div className="header__logo">
-        {/*<img src="/assets/logo.png" alt="Podcast Party Logo" />*/}
+     <Link to="/"><div className="header__logo">
+       <img src={logo} alt="Podcast Party Logo" />
       </div>
+      </Link> 
 
       <nav className="header__nav">
         <ul className="nav__links">
-          <li><Link to="/">Nosotros</Link></li>
-          <li><Link to="/episodes">Episodios</Link></li>
-          <li><a href="#comunidad">Comunidad</a></li>
+          <Link to="/episodes"><li>Episodios</li></Link>
+          <Link to="/episodes"><li>Registrarte</li></Link>
+          <Link to="/episodes"><li className='header__login'>Iniciar sesión</li></Link>
         </ul>
       </nav>
-      
-      <div className="header__auth">
-        <button className="btn btn--login">Sign In</button>
-        <button className="btn btn--register">Register</button>
-      </div>
     </header>
   );
 };
