cbc66ee7c8c109cde51d06511ab46f38fbff3b04
PMFrancisco <francisco.perez.munoz@gmail.com>
2024-11-17 19:41:42 +0100

    refactor(server): moved swagger to documentation files

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,59 +3,6 @@ const router = express.Router();
 const User = require("../models/user");
 const passport = require("passport");
 
-/**
- * @swagger
- * tags:
- *   name: Users
- *   description: User management
- */
-
-/**
- * @swagger
- * /users:
- *   get:
- *     summary: Get user details
- *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     responses:
- *       '200':
- *         description: User details retrieved successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 favorites:
- *                   type: array
- *                   items:
- *                     type: string
- *                   example: ["episode1", "episode2"]
- *                 lastListened:
- *                   type: string
- *                   example: "episode3"
- *       '404':
- *         description: User not found
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "User not found"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Internal server error"
- */
-
 router.get(
   "/",
   passport.authenticate("jwt", { session: false }),
@@ -75,54 +22,6 @@ router.get(
     }
   }
 );
-
-/**
- * @swagger
- * /users/lastListened/{podcastId}:
- *   post:
- *     summary: Update last listened podcast
- *     tags: [Users]
- *     security:
- *       - bearerAuth: []
- *     parameters:
- *       - in: path
- *         name: podcastId
- *         schema:
- *           type: string
- *         required: true
- *         description: The ID of the podcast to be marked as last listened
- *     responses:
- *       '200':
- *         description: Podcast updated successfully
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Podcast added to last listened"
- *       '400':
- *         description: Bad request
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Podcast ID is required"
- *       '500':
- *         description: Internal server error
- *         content:
- *           application/json:
- *             schema:
- *               type: object
- *               properties:
- *                 message:
- *                   type: string
- *                   example: "Internal server error"
- */
 
 router.post(
   "/lastListened/:podcastId",
